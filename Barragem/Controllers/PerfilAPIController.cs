40d3bcec72a26c4fcbc2d0e8ca7326079f57d5f7
alissoncoutinho <coutinho.alisson@gmail.com>
2019-05-29 00:18:56 -0300

    PerfilAPIController

---
+++ Barragem/Controllers/PerfilAPIController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Barragem.Context;
+using Barragem.Models;
+using WebMatrix.WebData;
+using Barragem.Class;
+
+namespace Barragem.Controllers
+{
+    public class PerfilAPIController : ApiController
+    {
+        private BarragemDbContext db = new BarragemDbContext();
+
+        // GET: api/JogoAPI
+        [Route("api/PerfilAPI/ResetarSenha")]
+        [ResponseType(typeof(void))]
+        [HttpGet]
+        public IHttpActionResult ResetarSenha(string email){
+            UserProfile user = null;
+            try{
+                user = db.UserProfiles.Where(u => u.email == email).FirstOrDefault();
+                if (user != null){
+                    if (String.IsNullOrEmpty(user.email)){
+                        return InternalServerError(new Exception("Este usuário não possui e-mail cadastrado. Por favor, entre em contato com o administrador."));
+                    } else {
+                        string confirmationToken = WebSecurity.GeneratePasswordResetToken(user.UserName);
+                        EnviarMailSenha(confirmationToken, user.nome, user.email);
+                        return StatusCode(HttpStatusCode.NoContent);
+                    }
+                }else{
+                    return InternalServerError(new Exception("Este usuário não existe."));
+                }
+            } catch (Exception ex) {
+                return InternalServerError(new Exception(ex.Message)); 
+            }finally{
+                if (db != null)
+                    db.Dispose();
+            }
+        }
+
+
+        private void EnviarMailSenha(string token, string nomeUsuario, string emailUsuario)
+        {
+            string strUrl = string.Format("http://www.rankingdetenis.com/Account/ConfirmaSenha/{1}", token);
+            string strConteudo = "<html> <head> </head> <body> Prezado(a) " + nomeUsuario + ", <br /> Você fez uma solicitação de reinicio de senha. <br />";
+            strConteudo += "Para continuar, clique no link abaixo: <br /> " + strUrl + " </body> </html>";
+
+            Mail email = new Mail();
+            email.SendEmail(emailUsuario, "recuperação de senha", strConteudo, Class.Tipos.FormatoEmail.Html);
+        }
+    }
+}
