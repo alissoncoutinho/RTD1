3b3058994bc983fac169b2d4b88825193646b9d5
alissoncoutinho <coutinho.alisson@gmail.com>
2020-01-27 22:01:11 -0300

    acertos

--- Barragem/Controllers/RankingAPIController.cs
+++ Barragem/Controllers/RankingAPIController.cs
@@ -202,11 +202,13 @@ namespace Barragem.Controllers
         {
             try
             {
-                var titulo = "Ranking atualizado e nova rodada gerada!";
+                var nomeRanking = db.BarragemView.Find(8).nome;
+                var titulo = nomeRanking + " Ranking atualizado e nova rodada gerada!";
                 var conteudo = "Clique aqui e entre em contato com seu adversário o mais breve possível e bom jogo.";
 
-                var fbmodel = new FirebaseNotificationModel() { to = "/topics/ranking8", notification = new NotificationModel() { title = titulo, body = conteudo }, data = new DataModel() { title = titulo, body = conteudo, type = "nova_rodada_aberta" } };
+                var fbmodel = new FirebaseNotificationModel() { to = "/topics/ranking8", notification = new NotificationModel() { title = titulo, body = conteudo }, data = new DataModel() { title = titulo, body = conteudo, type = "nova_rodada_aberta", idRanking = 8 } };
                 new FirebaseNotification().SendNotification(fbmodel);
+                                
             }
             catch (Exception e)
             {
--- Barragem/Scripts/barragem.js
+++ Barragem/Scripts/barragem.js
@@ -29,29 +29,26 @@ $(document).ready(function () {
                 text: 'Sim',
                 btnClass: 'btn-primary',
                 action: function () {
-                    if (this.$target.data("link") == "/Rodada/FecharRodada/8417") {
-                        $.alert('Vai executar!');
-                    }
                     location.href = this.$target.data("link");
                 }
             },
-            cancelar: function () {
-
-            }
+            cancelar: function () { }
         }
     });
 
     $(".confirmExclusaoRodada").confirm({
         title: "Confirmar Operação",
-        text: "Tem certeza que deseja EXCLUIR PERMANENTEMENTE esta rodada, os seus respectivos jogos e o ranking caso exista?",
-        confirm: function (button) {
-            window.location.href = button.data("link");
-        },
-        cancel: function (button) {
-
-        },
-        confirmButton: "Sim",
-        cancelButton: "Não"
+        content: "Tem certeza que deseja EXCLUIR PERMANENTEMENTE esta rodada, os seus respectivos jogos e o ranking caso exista?",
+        buttons: {
+            sim: {
+                text: 'Sim',
+                btnClass: 'btn-primary',
+                action: function () {
+                    location.href = this.$target.data("link");
+                }
+            },
+            cancelar: function () { }
+        }
     });
     
 });
