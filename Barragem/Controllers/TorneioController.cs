8b090f316b1d5457a027477387c6f29caa8d29e0
Elton Araujo de Castro <eltonaraujodecastro@gmail.com>
2021-11-12 16:16:34 -0300

    Filtro de nome de jogador ta tela de inscricao de torneio

--- Barragem/Controllers/TorneioController.cs
+++ Barragem/Controllers/TorneioController.cs
@@ -955,7 +955,7 @@ namespace Barragem.Controllers
         }
 
         [Authorize(Roles = "admin,organizador,adminTorneio")]
-        public ActionResult EditInscritos(int torneioId, int filtroClasse = 0, string Msg = "")
+        public ActionResult EditInscritos(int torneioId, int filtroClasse = 0, string filtroJogador = "", string Msg = "")
         {
 
             List<InscricaoTorneio> inscricao = db.InscricaoTorneio.Where(i => i.torneioId == torneioId).ToList();
@@ -964,6 +964,10 @@ namespace Barragem.Controllers
             {
                 inscricao = inscricao.Where(i => i.classe == filtroClasse).ToList();
                 ViewBag.CabecasDeChave = getOpcoesCabecaDeChave(filtroClasse);
+            }
+            if (filtroJogador != "")
+            {
+                inscricao = inscricao.Where(i => i.participante.nome.ToUpper().Contains(filtroJogador.ToUpper())).ToList();
             }
             ViewBag.descricaoTipoDesconto = torneio.descontoPara;
             if (torneio.valorDescontoFederado > torneio.valorSocio)
