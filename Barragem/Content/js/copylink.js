3f70e0ce8b008bae9c61d79f563a128bc47250af
Elton Araujo de Castro <eltonaraujodecastro@gmail.com>
2021-12-18 12:18:15 -0300

    versao com torneio em andamento funcionando

--- Barragem/Content/js/copylink.js
+++ Barragem/Content/js/copylink.js
@@ -1,5 +1,6 @@
 let copyLink = document.getElementById('copy-link');
-let copyLink2 = document.getElementById('copy-link2');
+var copyLinkUrl = copyLink.getAttribute('data-link');
+
 function copyTextToClipboard(text) {
     if (!navigator.clipboard) {
       fallbackCopyTextToClipboard(text);
@@ -12,46 +13,26 @@ function copyTextToClipboard(text) {
     });
   }
 
-  copyLink.addEventListener('click', function(event) {
-    copyTextToClipboard('https://fellipedev.com');
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
+copyLink.addEventListener('click', function(event) {
+copyTextToClipboard(copyLinkUrl);
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+    })
       
-      Toast.fire({
-        icon: 'success',
-        title: 'Link copiado com sucesso'
-      })
-  });
+    Toast.fire({
+    icon: 'success',
+    title: 'Link copiado com sucesso'
+    })
+});
 
-  copyLink2.addEventListener('click', function(event) {
-    copyTextToClipboard('https://google.com');
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-      
-      Toast.fire({
-        icon: 'success',
-        title: 'Link copiado com sucesso'
-      })
-  });
-
-  copyTextToClipboard();
+copyTextToClipboard(copyLinkUrl);
 
   
