d568f656b11c77519cb366cb2803bc09169180f2
João Missio <joao-missio@hotmail.com>
2022-03-27 10:57:41 -0300

    roteamento para pagina landing page

--- Barragem/App_Start/RouteConfig.cs
+++ Barragem/App_Start/RouteConfig.cs
@@ -26,6 +26,24 @@ namespace Barragem
            );
 
             routes.MapRoute(
+               name: "LandingPageLiga",
+               url: "liga-{key}",
+               defaults: new { controller = "LandingPage", action = "LigaRedirect", key = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "LandingPageCircuito",
+               url: "circuito-{key}",
+               defaults: new { controller = "LandingPage", action = "CircuitoRedirect", key = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "LandingPageFederacao",
+               url: "federacao-{key}",
+               defaults: new { controller = "LandingPage", action = "FederacaoRedirect", key = UrlParameter.Optional }
+           );
+            
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Barragem/Controllers/LandingPageController.cs
+++ Barragem/Controllers/LandingPageController.cs
@@ -8,10 +8,24 @@ namespace Barragem.Controllers
 {
     public class LandingPageController : Controller
     {
-        // GET: LandingPage
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
             return View();
+        }
+
+        public ActionResult LigaRedirect(string key)
+        {
+            return RedirectToAction("Index", "LandingPage", new { id = 0 });
+        }
+        
+        public ActionResult CircuitoRedirect(string key)
+        {
+            return RedirectToAction("Index", "LandingPage", new { id = 0 });
+        }
+
+        public ActionResult FederacaoRedirect(string key)
+        {
+            return RedirectToAction("Index", "LandingPage", new { id = 0 });
         }
     }
 }
