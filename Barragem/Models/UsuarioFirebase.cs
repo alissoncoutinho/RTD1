880fa8d92064826332ba772aee68eafb965a8f43
João Missio <joao-missio@hotmail.com>
2022-05-04 21:07:49 -0300

    inicio implementação

---
+++ Barragem/Models/UsuarioFirebase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Barragem.Models
+{
+    public class UsuarioFirebase
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Token { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+
+        public virtual UserProfile UserProfile { get; set; }
+    }
+}
