8fb6bd35c9212f6b0c45b7230270a722bbf205a2
João Missio <joao-missio@hotmail.com>
2022-06-01 19:01:45 -0300

    limitação campos

--- sql/Tabela_DadosBancariosTorneio.sql
+++ sql/Tabela_DadosBancariosTorneio.sql
@@ -2,11 +2,21 @@
 ALTER TABLE [dbo].[Torneio]
 	ADD 
 		[NomeBanco] varchar(50)  NULL,
-		[Agencia] varchar(10)  NULL,
-		[ContaCorrente] varchar(20)  NULL,
+		[Agencia] varchar(50)  NULL,
+		[ContaCorrente] varchar(100)  NULL,
 		[ChavePix] varchar(100)  NULL,
-		[CpfConta] varchar(20)  NULL,
-		[NomeOrganizador] varchar(50)  NULL,
-		[ContatoOrganizador] varchar(20)  NULL
+		[CpfConta] varchar(30)  NULL,
+		[NomeOrganizador] varchar(100)  NULL,
+		[ContatoOrganizador] varchar(50)  NULL
 	
 GO
+
+
+ALTER TABLE [dbo].[Torneio] ALTER COLUMN [NomeBanco] varchar(50)  NULL;
+ALTER TABLE [dbo].[Torneio] ALTER COLUMN [Agencia] varchar(50)  NULL;
+ALTER TABLE [dbo].[Torneio] ALTER COLUMN [ContaCorrente] varchar(100)  NULL;
+ALTER TABLE [dbo].[Torneio] ALTER COLUMN [ChavePix] varchar(100)  NULL;
+ALTER TABLE [dbo].[Torneio] ALTER COLUMN [CpfConta] varchar(30)  NULL;
+ALTER TABLE [dbo].[Torneio] ALTER COLUMN [NomeOrganizador] varchar(100)  NULL;
+ALTER TABLE [dbo].[Torneio] ALTER COLUMN [ContatoOrganizador] varchar(50)  NULL;
+
