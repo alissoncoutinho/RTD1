e5355c73ba37c77f82cdc88756278fb7934e7ffc
João Missio <joao-missio@hotmail.com>
2022-06-09 21:56:52 -0300

    remoção sorteios geração mata mata

--- Barragem/Class/MontaJogosTorneioRegraCBT.cs
+++ Barragem/Class/MontaJogosTorneioRegraCBT.cs
@@ -146,14 +146,7 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom4Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao2 = 0;
-        private int posicao3 = 0;
-        public MontaJogosTorneioRegraCBTCom4Grupos()
-        {
-            posicao2 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao3 = (posicao2 == 3) ? 4 : 3;
-        }
-
+        //TODO: Montagem dos jogos estranha...
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             // 2|3 onde 2 é a colocação dos primeiros colocados em cada grupo e 3 é o segundo colocado do grupo do 3o colocado entre os primeiros;
@@ -163,9 +156,9 @@ namespace Barragem.Class
                 case 1:
                     return "1|3";
                 case 2:
-                    return posicao2 + "|2";
+                    return "4|2";
                 case 3:
-                    return posicao3 + "|1";
+                    return "3|1";
                 case 4:
                     return "2|4";
                 default:
@@ -176,13 +169,6 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom5Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao4 = 0;
-        private int posicao5 = 0;
-        public MontaJogosTorneioRegraCBTCom5Grupos()
-        {
-            posicao4 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao5 = (posicao4 == 3) ? 4 : 3;
-        }
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             // 2|3 onde 2 é a colocação dos primeiros colocados em cada grupo e 3 é o segundo colocado do grupo do 3o colocado entre os primeiros;
@@ -196,9 +182,9 @@ namespace Barragem.Class
                 case 3:
                     return "3-S|0";
                 case 4:
-                    return posicao4 + "|0";
+                    return "4|0";
                 case 5:
-                    return posicao5 + "|0";
+                    return "3|0";
                 case 6:
                     return "5|0";
                 case 7:
@@ -213,13 +199,6 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom6Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao4 = 0;
-        private int posicao5 = 0;
-        public MontaJogosTorneioRegraCBTCom6Grupos()
-        {
-            posicao4 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao5 = (posicao4 == 3) ? 4 : 3;
-        }
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             
@@ -234,9 +213,9 @@ namespace Barragem.Class
                 case 3:
                     return "6|3";
                 case 4:
-                    return posicao4 + "|0";
+                    return "4|0";
                 case 5:
-                    return posicao5 + "|0";
+                    return "3|0";
                 case 6:
                     return "5|6";
                 case 7:
@@ -251,19 +230,6 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom7Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao4 = 0;
-        private int posicao5 = 0;
-        private int posicao3 = 0;
-        private int posicao6 = 0;
-        public MontaJogosTorneioRegraCBTCom7Grupos()
-        {
-            posicao4 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao5 = (posicao4 == 3) ? 4 : 3;
-
-            posicao3 = new Random().Next(5, 7); // sorteia entre 5 e 6
-            posicao6 = (posicao3 == 5) ? 6 : 5;
-        }
-
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             
@@ -276,13 +242,13 @@ namespace Barragem.Class
                 case 2:
                     return "7|2";
                 case 3:
-                    return posicao3 + "|3";
+                    return "6|3";
                 case 4:
-                    return posicao4 + "|5";
+                    return "4|5";
                 case 5:
-                    return posicao5 + "|4";
+                    return "3|4";
                 case 6:
-                    return posicao6 + "|6";
+                    return "5|6";
                 case 7:
                     return "1-S|7";
                 case 8:
@@ -295,33 +261,6 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom8Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao2 = 0;
-        private int posicao3 = 0;
-        private int posicao4 = 0;
-        private int posicao5 = 0;
-        private int posicao6 = 0;
-        private int posicao7 = 0;
-        public MontaJogosTorneioRegraCBTCom8Grupos()
-        {
-            posicao4 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao5 = (posicao4 == 3) ? 4 : 3;
-
-            List<int> listNumbers = new List<int>();
-            int number;
-            for (int i = 0; i < 4; i++)
-            {
-                do
-                {
-                    number = new Random().Next(5, 9);
-                } while (listNumbers.Contains(number));
-                listNumbers.Add(number);
-            }
-
-            posicao2 = listNumbers[0];
-            posicao3 = listNumbers[1];
-            posicao6 = listNumbers[2];
-            posicao7 = listNumbers[3];
-        }
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             
@@ -332,17 +271,17 @@ namespace Barragem.Class
                 case 1:
                     return "1|8";
                 case 2:
-                    return posicao2 + "|2";
+                    return "7|2";
                 case 3:
-                    return posicao3 + "|3";
+                    return "6|3";
                 case 4:
-                    return posicao4 + "|5";
+                    return "4|5";
                 case 5:
-                    return posicao5 + "|6";
+                    return "3|6";
                 case 6:
-                    return posicao6 + "|4";
+                    return "5|4";
                 case 7:
-                    return posicao7 + "|1";
+                    return "8|1";
                 case 8:
                     return "2|7";
                 default:
@@ -353,36 +292,6 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom9Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao3  = 0;
-        private int posicao4  = 0;
-        private int posicao5  = 0;
-        private int posicao8  = 0;
-        private int posicao9  = 0;
-        private int posicao12 = 0;
-        private int posicao13 = 0;
-
-        public MontaJogosTorneioRegraCBTCom9Grupos()
-        {
-            posicao8 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao9 = (posicao8 == 3) ? 4 : 3;
-
-            List<int> listNumbers = new List<int>();
-            int number;
-            for (int i = 0; i < 5; i++)
-            {
-                do
-                {
-                    number = new Random().Next(5, 10);
-                } while (listNumbers.Contains(number));
-                listNumbers.Add(number);
-            }
-
-            posicao3 = listNumbers[0];
-            posicao4 = listNumbers[1];
-            posicao5 = listNumbers[2];
-            posicao12 = listNumbers[3];
-            posicao13 = listNumbers[4];
-        }
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             
@@ -395,27 +304,27 @@ namespace Barragem.Class
                 case 2:
                     return "8-S|3";
                 case 3:
-                    return posicao3 + "|0";
+                    return "9|0";
                 case 4:
-                    return posicao4 + "|0";
+                    return "6|0";
                 case 5:
-                    return posicao5 + "|0";
+                    return "7|0";
                 case 6:
                     return "2-S|0";
                 case 7:
                     return "5-S|0";
                 case 8:
-                    return posicao8 + "|0";
+                    return "4|0";
                 case 9:
-                    return posicao9 + "|0";
+                    return "3|0";
                 case 10:
                     return "4-S|0";
                 case 11:
                     return "1-S|0";
                 case 12:
-                    return posicao12 + "|0";
+                    return "8|0";
                 case 13:
-                    return posicao13 + "|0";
+                    return "5|0";
                 case 14:
                     return "9-S|0";
                 case 15:
@@ -430,36 +339,6 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom10Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao3 = 0;
-        private int posicao4 = 0;
-        private int posicao5 = 0;
-        private int posicao8 = 0;
-        private int posicao9 = 0;
-        private int posicao12 = 0;
-        private int posicao13 = 0;
-
-        public MontaJogosTorneioRegraCBTCom10Grupos()
-        {
-            posicao8 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao9 = (posicao8 == 3) ? 4 : 3;
-
-            List<int> listNumbers = new List<int>();
-            int number;
-            for (int i = 0; i < 5; i++)
-            {
-                do
-                {
-                    number = new Random().Next(5, 10);
-                } while (listNumbers.Contains(number));
-                listNumbers.Add(number);
-            }
-
-            posicao3 = listNumbers[0];
-            posicao4 = listNumbers[1];
-            posicao5 = listNumbers[2];
-            posicao12 = listNumbers[3];
-            posicao13 = listNumbers[4];
-        }
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             
@@ -472,27 +351,27 @@ namespace Barragem.Class
                 case 2:
                     return "8-S|3";
                 case 3:
-                    return posicao3 + "|0";
+                    return "9|0";
                 case 4:
-                    return posicao4 + "|0";
+                    return "6|0";
                 case 5:
-                    return posicao5 + "|0";
+                    return "7|0";
                 case 6:
                     return "2-S|10";
                 case 7:
                     return "5-S|0";
                 case 8:
-                    return posicao8 + "|0";
+                    return "4|0";
                 case 9:
-                    return posicao9 + "|0";
+                    return "3|0";
                 case 10:
                     return "4-S|0";
                 case 11:
                     return "1-S|0";
                 case 12:
-                    return posicao12 + "|0";
+                    return "8|0";
                 case 13:
-                    return posicao13 + "|0";
+                    return "5|0";
                 case 14:
                     return "10|9";
                 case 15:
@@ -507,36 +386,6 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom11Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao3 = 0;
-        private int posicao4 = 0;
-        private int posicao5 = 0;
-        private int posicao7 = 0;
-        private int posicao8 = 0;
-        private int posicao9 = 0;
-        private int posicao12 = 0;
-        private int posicao13 = 0;
-        public MontaJogosTorneioRegraCBTCom11Grupos() {
-            posicao8 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao9 = (posicao8 == 3) ? 4 : 3;
-
-            List<int> listNumbers = new List<int>();
-            int number;
-            for (int i = 0; i < 6; i++)
-            {
-                do
-                {
-                    number = new Random().Next(5, 12);
-                } while ((listNumbers.Contains(number)) || (number == 10));
-                listNumbers.Add(number);
-            }
-
-            posicao3 = listNumbers[0];
-            posicao4 = listNumbers[1];
-            posicao5 = listNumbers[2];
-            posicao7 = listNumbers[3];
-            posicao12 = listNumbers[4];
-            posicao13 = listNumbers[5];
-        }
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             // 2|3 onde 2 é a colocação dos primeiros colocados em cada grupo e 3 é o segundo colocado do grupo do 3o colocado entre os primeiros;
@@ -548,27 +397,27 @@ namespace Barragem.Class
                 case 2:
                     return "8-S|3";
                 case 3:
-                    return posicao3 + "|0";
+                    return "9|0";
                 case 4:
-                    return posicao4 + "|0";
+                    return "6|0";
                 case 5:
-                    return posicao5 + "|0";
+                    return "7|0";
                 case 6:
                     return "2-S|10";
                 case 7:
-                    return posicao7 + "|5";
+                    return "11|5";
                 case 8:
-                    return posicao8 + "|0";
+                    return "4|0";
                 case 9:
-                    return posicao9 + "|0";
+                    return "3|0";
                 case 10:
                     return "4-S|0";
                 case 11:
                     return "1-S|11";
                 case 12:
-                    return posicao12 + "|0";
+                    return "8|0";
                 case 13:
-                    return posicao13 + "|0";
+                    return "5|0";
                 case 14:
                     return "10|9";
                 case 15:
@@ -583,39 +432,6 @@ namespace Barragem.Class
 
     public class MontaJogosTorneioRegraCBTCom12Grupos : MontaJogosTorneioRegraCBT
     {
-        private int posicao3 = 0;
-        private int posicao4 = 0;
-        private int posicao5 = 0;
-        private int posicao7 = 0;
-        private int posicao8 = 0;
-        private int posicao9 = 0;
-        private int posicao10 = 0;
-        private int posicao12 = 0;
-        private int posicao13 = 0;
-        public MontaJogosTorneioRegraCBTCom12Grupos()
-        {
-            posicao8 = new Random().Next(3, 5); // sorteia entre 3 e 4
-            posicao9 = (posicao8 == 3) ? 4 : 3;
-
-            List<int> listNumbers = new List<int>();
-            int number;
-            for (int i = 0; i < 7; i++)
-            {
-                do
-                {
-                    number = new Random().Next(5, 13);
-                } while ((listNumbers.Contains(number)) || (number == 10));
-                listNumbers.Add(number);
-            }
-
-            posicao3 = listNumbers[0];
-            posicao4 = listNumbers[1];
-            posicao5 = listNumbers[2];
-            posicao7 = listNumbers[3];
-            posicao10 = listNumbers[4];
-            posicao12 = listNumbers[5];
-            posicao13 = listNumbers[6];
-        }
         public override string GetPosicaoPorOrdemJogo(int ordemJogo)
         {
             // 2|3 onde 2 é a colocação dos primeiros colocados em cada grupo e 3 é o segundo colocado do grupo do 3o colocado entre os primeiros;
@@ -627,27 +443,27 @@ namespace Barragem.Class
                 case 2:
                     return "8-S|3";
                 case 3:
-                    return posicao3 + "|12";
+                    return "9|12";
                 case 4:
-                    return posicao4 + "|0";
+                    return "6|0";
                 case 5:
-                    return posicao5 + "|0";
+                    return "7|0";
                 case 6:
                     return "2-S|10";
                 case 7:
-                    return posicao7 + "|5";
+                    return "11|5";
                 case 8:
                     return "4|0";
                 case 9:
                     return "3|0";
                 case 10:
-                    return posicao10 + "|4";
+                    return "12|4";
                 case 11:
                     return "1-S|11";
                 case 12:
-                    return posicao12 + "|0";
+                    return "8|0";
                 case 13:
-                    return posicao13 + "|0";
+                    return "5|0";
                 case 14:
                     return "10|9";
                 case 15:
